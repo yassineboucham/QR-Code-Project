2ecf8e3282a9a68ceba5ff326ce3d643f46f59a4
yassineboucham <yassineboucham2002@gmail.com>
2025-01-28 16:49:47 +0100

    Use the qr-image npm package to turn the user entered URL into a QR code image.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 */
 import inquirer from 'inquirer';
 import qr from 'qr-image';
+import fs from 'fs';
 
 inquirer
   .prompt([
@@ -17,7 +18,8 @@ inquirer
   .then((answers) => {
     // Use user feedback for... whatever!!
     var url = answers.URL;
-
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('QR.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
